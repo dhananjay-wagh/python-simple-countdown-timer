999bff6361a7fac48ca0f2561ff10b6046f2157b
Dhananjay Wagh <dhananjaywagh426@gmail.com>
2026-03-03 18:14:17 +0530

    added hours & minutes

--- countdown.py
+++ countdown.py
@@ -1,20 +1,12 @@
 import time
 
-my_time = int(input("Enter the countdwon Duration : "))
+my_time = int(input("Enter the countdwon Duration (int seconds) : "))
 
-#Method 1 (simple one)
-# for x in range( 0 , my_time):
-#     print(x)
-#     time.sleep(1)
-
-#Method 2 (Reversed)
-# for x in range( my_time , 0 , -1):
-#     print(x)
-#     time.sleep(1)
-
-#Method 1 (Reversed with reverses function)
-for x in reversed(range( 0 , my_time)):
-    print(x)
+for x in range( my_time , 0 , -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 print("Times up!!")
